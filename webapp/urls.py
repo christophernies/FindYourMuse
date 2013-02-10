9784d42b6439721bb6e63a163dae51a536644d10
William Prin <waprin@gmail.com>
2013-02-10 00:13:53 -0500

    fix some syntax errors

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.http import HttpResponse
-from webapp.views import FilterByMuse, ScrapeGilt
+from webapp.views import FilterByMuse
 
 # Uncomment the next two lines to enable the admin:
- from django.contrib import admin
- admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'', include('singly.urls')),
     url(r'^$', 'index', name='index'),
     url('^results$', FilterByMuse),
-    (url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
