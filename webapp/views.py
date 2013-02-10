f1fd7b00bb8afae9f9f99518574082abe2e1b500
Christopher Nies <christophernies@gmail.com>
2013-02-09 19:23:54 -0500

    fixed indent

--- webapp/views.py
+++ webapp/views.py
@@ -25,9 +25,10 @@ def index(request, template='index.html'):
         'FitBit',
         'Email'
     ]
-
-if request.user.is_authenticated():
-	user_profile = request.user.get_profile()
-	profiles = user_profile.profiles
-	response = render_to_response(template, locals(), context_instance=RequestContext(request))
-return response
+    if request.user.is_authenticated():
+        user_profile = request.user.get_profile()
+        profiles = user_profile.profiles
+    response = render_to_response(
+            template, locals(), context_instance=RequestContext(request)
+        )
+    return response
