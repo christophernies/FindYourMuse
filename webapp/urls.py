b8a1ed32e42a5e23050f1a9cdbad7777e767abfc
William Prin <waprin@gmail.com>
2013-02-10 00:10:16 -0500

    add admin and also fix urls narrowing scope issue

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.http import HttpResponse
-from webapp.views import FilterByMuse
+from webapp.views import FilterByMuse, ScrapeGilt
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+ from django.contrib import admin
+ admin.autodiscover()
 
-urlpatterns = patterns('webapp.views',
+urlpatterns = patterns('',
     url(r'', include('singly.urls')),
     url(r'^$', 'index', name='index'),
-    ('^results$', FilterByMuse),
+    url('^results$', FilterByMuse),
+    url('^gilt$', ScrapeGilt)
 )
 
