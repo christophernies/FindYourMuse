2c10793b5998bab1bb73e3bd5299da33ed073496
Christopher Nies <nies@mint>
2013-02-17 13:08:33 -0500

    removing credentials

--- webapp/login_credentials.py
+++
@@ -1,4 +0,0 @@
-cloudmine_app = 'fc4b2b7a225543e796c753590a2c5a1d'
-cloudmine_api_key = '84e8b6c7379b4952af94f34c7ed283bc'
-hearst_api_key = '8qgkp2gtvrbudmfq2mvc2qrv'
-gilt_api_key = '90d190b533b721fce5717c04ff32fe7e'
